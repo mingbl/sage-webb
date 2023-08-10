cc7c7c215211b586daa9912e325380e4bde9a1ed
mingbl <61300438+mingbl@users.noreply.github.com>
2023-08-10 19:03:46 +1000

    html structure started

--- webbViewer/webbViewer.js
+++ webbViewer/webbViewer.js
@@ -11,16 +11,34 @@ var webbViewer = SAGE2_App.extend({
 
         // `this.element` is created and assigned based on the above SAGE2Init.
         // In this case, this.element is a div
+        var div = this.element
 
         // Assign the div node an id.
         // `this.id` is a unique identifier created for this instance of the app
         this.element.id = "div" + this.id;
 
         // Fill the element with text "Hello World"
-        this.element.textContent = "This is not the James Webb Telescope Viewer";
+        // this.element.textContent = "This is not the James Webb Telescope Viewer";
 
         // Makes the app background white
-        this.element.style.background = "green";
+        this.element.style.background = "white";
+
+        var h1 = document.createElement("h1")
+        h1.innerHTML = "Heading"
+        div.appendChild(h1)
+
+        var p = document.createElement("p")
+        p.innerHTML = "Paragraph"
+        div.appendChild(p)
+
+        var p2 = document.createElement("p")
+        p2.innerHTML = "Paragraph"
+        div.appendChild(p2)
+
+        var img = document.createElement("img")
+        img.src = "https://placehold.co/600x400/png"
+        img.alt = "picture"
+        div.appendChild(img)
     },
 
 
