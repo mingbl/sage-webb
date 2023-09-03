dd4ee72917e141c5f9444e1104877639a4948a9b
mingbl <61300438+mingbl@users.noreply.github.com>
2023-09-03 17:32:38 +1000

    The Good Update
    Images and text parts line up in 20 columns to match the cave's 20 horizontal monitors
    
    the 'images' array of objects (images + title and description, and the width and height (important))
    
    gets turned into 'imageSets' - an array of: the image + the number of columns it takes up
    
    the first image set is displayed on the screen, and the 'imageSetCounter' goes up by 1, to keep track of what set has been displayed
    
    then every 10 seconds (configurable), the screen is re-rendered with the next image set, and the counter goes up by 1

--- webbViewer/webbViewer.js
+++ webbViewer/webbViewer.js
@@ -1,27 +1,33 @@
 var webbViewer = SAGE2_App.extend({
-    // The init function will be called during application creation
-    // The signature must include the data parameter which will be passed to the parent through SAGE2Init()
     init: function(data) {
-
-        // Create the application with a div node
         this.SAGE2Init("div", data);
-
-        // `this.element` is created and assigned based on the above SAGE2Init.
-        // In this case, this.element is a div
-
-        // Assign the div node an id.
-        // `this.id` is a unique identifier created for this instance of the app
         this.element.id = "div" + this.id;
 
-        var imageDirectory = "local_images/"
+        this.redraw = true
+        this.log("Webb Viewer started")
 
-        var container = this.element
+        const imageDirectory = "local_images/"
+
+        const container = this.element
         this.element.classList.add("container")
-
-        var imageDirectory = "./webbViewer/local_images/"
-        var container = document.querySelector("div.container")
+        
+        this.resizeEvents = "continuous"
         
         const images = [
+            {
+                title: "The Pillars of Creation (Webb NIRCam Image)",
+                description: "This Webb image of the “Pillars of Creation” has layers of semi-opaque, rusty red gas and dust that start at the bottom left and go toward the top right. There are three prominent pillars rising toward the top right. The left pillar is the largest and widest. The peaks of the second and third pillars are set off in darker shades of brown and have red outlines. Peeking through the layers of gas and dust is the background, set in shades of blue and littered with tiny yellow and blue stars. Many of the tips of the pillars appear tinged with what looks like lava. There are also tiny red dots at the edges of the pillars, which are newly born stars.",
+                url: "https://cdn.discordapp.com/attachments/716936586180165682/1147782441784836116/6.webp",
+                width: 3547,
+                height: 6144
+            },
+            {
+                title: "The Phantom Galaxy Across the Spectrum",
+                description: "This image shows Webb near-infrared data combined with optical data from Hubble. Lacy red filaments spiraling out of the center of the galaxy are overlaid over a black field speckled with stars. The center of the galaxy glows in a pale color. The red filaments contain pops of bright pink, and some blue stars are visible in the background. The red color is dust, lighter oranges in the dust mean that dust is hotter. The young stars sprinkled through the arms and around the core of the galaxy are blue. Heavier older stars nearer the center of the galaxy are cyan and green and contribute to its glow. The pink pops of color are star forming regions. ",
+                url: "https://cdn.discordapp.com/attachments/716936586180165682/1147782903879700490/52324839010_5fd541e2f2_k.jpg",
+                width: 2048,
+                height: 1273
+            },
             {
                 title: "Heading",
                 description: "Discovered by Hubble, Earendel is the farthest star ever detected. It existed in the first billion years after the big bang! The James Webb Space Telescope now shows it to be a massive B-type star, more than twice as hot as our Sun and about a million times more luminous. It’s only detectable thanks to its alignment with a galaxy cluster between Earendel and us. The cluster’s gravity bends light, magnifying what is behind it — in the case of a star-sized object like Earendel, by a factor of at least 4000. Based on the colors of the light of Earendel, astronomers think it may have a cooler companion star. Webb is also able to see other details in Earendel’s host galaxy, the Sunrise Arc — the most highly magnified galaxy yet detected in the universe’s first billion years. Those features include both young star-forming regions and older, established star clusters as small as 10 light-years across.",
@@ -31,7 +37,7 @@ var webbViewer = SAGE2_App.extend({
                 height: 1193
             },
             {
-                title: "Heading2",
+                title: "Headin asdas dasdasd asdg2",
                 description: "Discovered by Hubble, Earendel is the farthest star ever detected. It existed in the first billion years after the big bang! The James Webb Space Telescope now shows it to be a massive B-type star, more than twice as hot as our Sun and about a million times more luminous. It’s only detectable thanks to its alignment with a galaxy cluster between Earendel and us. The cluster’s gravity bends light, magnifying what is behind it — in the case of a star-sized object like Earendel, by a factor of at least 4000. Based on the colors of the light of Earendel, astronomers think it may have a cooler companion star. Webb is also able to see other details in Earendel’s host galaxy, the Sunrise Arc — the most highly magnified galaxy yet detected in the universe’s first billion years. Those features include both young star-forming regions and older, established star clusters as small as 10 light-years across.",
                 url: "https://cdn.discordapp.com/attachments/1134060514994028594/1139348535406506137/53104943445_9398264921_k.jpg",
                 // url: `${imageDirectory}2.jpg`,
@@ -58,12 +64,22 @@ var webbViewer = SAGE2_App.extend({
                 title: "Heading5",
                 description: "Discovered by Hubble, Earendel is the farthest star ever detected. It existed in the first billion years after the big bang! The James Webb Space Telescope now shows it to be a massive B-type star, more than twice as hot as our Sun and about a million times more luminous. It’s only detectable thanks to its alignment with a galaxy cluster between Earendel and us. The cluster’s gravity bends light, magnifying what is behind it — in the case of a star-sized object like Earendel, by a factor of at least 4000. Based on the colors of the light of Earendel, astronomers think it may have a cooler companion star. Webb is also able to see other details in Earendel’s host galaxy, the Sunrise Arc — the most highly magnified galaxy yet detected in the universe’s first billion years. Those features include both young star-forming regions and older, established star clusters as small as 10 light-years across.",
                 url: "https://cdn.discordapp.com/attachments/716936586180165682/1146979943486586972/The_Sixth_Doctor_Regenerates_Time_and_the_Rani_Doctor_Who_-_YouTube_-_0_05.jpeg",
+                // url: `${imageDirectory}5.jpg`,
                 width: 1402,
                 height: 1080
             }
         ]
         
-        const columns = 20, columnWidth = 100 / columns
+        const columns = 20, viewerWidth = 4000, viewerHeight = 440
+        
+        /**
+         * Time in milliseconds before the image set is replaced
+         */
+        const imageLifespan = 10000
+        
+        const viewerAspectRatio = viewerWidth / viewerHeight
+        const imageSets = []
+        let imageSetCounter = 0
         
         /**
          * Create an element, add class name, and append to a parent element
@@ -79,13 +95,15 @@ var webbViewer = SAGE2_App.extend({
             return newElement
         }
         
-        function createShowcase(image, numOfRequiredColumns, datab) {
-            
-            const div = createComponent("div", "showcase", container)
-            div.style.width = `${(numOfRequiredColumns) * columnWidth}%`
-        
-            const textPart = createComponent("div", "text-part", div)
-            textPart.style.width = `${(100 / numOfRequiredColumns)}%`
+        /**
+         * Create a showcase (text part + image part) and append to the container
+         * @param {object} image - an image object {} in the images [] array
+         * @param {integer} numOfColumns - number of columns necessary to display the whole image (excluding text part)
+         */
+        function createShowcase(image, numOfColumns) {
+            const textPart = createComponent("div", "text-part", container)
+            textPart.style.width = `${100 / columns}%`
+            textPart.style.setProperty("animation-duration", `${imageLifespan / 1000}s`)
             
             const title = createComponent("h1", "title", textPart)
             title.innerHTML = `${image.title}`
@@ -93,48 +111,74 @@ var webbViewer = SAGE2_App.extend({
             const description = createComponent("p", "description", textPart)
             description.innerHTML = image.description
             
-            const imagePart = createComponent("div", "image-part", div)
+            const imagePart = createComponent("div", "image-part", container)
             imagePart.style.backgroundImage = `url(${image.url})`
-            imagePart.style.width = `${(100 / numOfRequiredColumns * (numOfRequiredColumns - 1))}%`
+            imagePart.style.width = `${numOfColumns * (100 / columns)}%`
+            imagePart.style.setProperty("animation-duration", `${imageLifespan / 1000}s`)
         }
         
-        function getShowcaseWidth(imageCols) {
-            return numOfRequiredColumns + 1
-        }
-        
-        function renderDisplay() {
-            const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-            const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-            const viewerAspectRatio = vw / vh
-        
-            let columnsUsed = 0
-            for (let i = 0; i < images.length; i++) {
-                const image = images[i]
+        /**
+         * Arrange/group images {} in the 'images' [] array into 'imageSets' [].
+         * 
+         * This algorithm uses the aspect ratio of the image to calculate 
+         * how many columns are required to display this image and its text part.
+         */
+        function arrangeImageSets() {
+            let imageCounter = 0, columnsUsed = 0, imageSetCounter = 0
+            while (images.length > imageCounter) {
+                const image = images[imageCounter]
         
                 const imageAspectRatio = image.width / image.height
         
                 const numOfColumns = Math.ceil((columns / viewerAspectRatio) * imageAspectRatio)
                 const numOfRequiredColumns = numOfColumns + 1 // Image + Text
         
-                if (columnsUsed + numOfRequiredColumns > columns) return
+                if (columnsUsed + numOfRequiredColumns > columns) {
+                    columnsUsed = 0
+                    imageSetCounter++
+                }
         
                 columnsUsed += numOfRequiredColumns
-                const debugData = {
-                    vw: vw,
-                    vh: vh,
-                    columns: columns,
-                    viewerAspectRatio: viewerAspectRatio,
-                    columnsUsed: columnsUsed,
-                    imageAspectRatio: imageAspectRatio,
-                    numOfColumns: numOfColumns,
-                    numOfRequiredColumns: numOfRequiredColumns
-                }
-                createShowcase(image, numOfRequiredColumns, JSON.stringify(debugData))
-                console.log(debugData)
+        
+                if (!imageSets[imageSetCounter]) imageSets[imageSetCounter] = []
+                imageSets[imageSetCounter].push({
+                    image: image,
+                    numOfColumns: numOfColumns
+                })
+        
+                imageCounter++
             }
         }
         
+        /**
+         * Render the current image set.
+         * 
+         * (set by imageSetCounter)
+         */
+        function renderDisplay() {
+            let imageSetCounterModulo = imageSetCounter % imageSets.length
+            console.log(`Rendering set ${imageSetCounterModulo}`)
+            const imageSet = imageSets[imageSetCounterModulo]
+            
+            // Clear display
+            while (container.firstChild) container.removeChild(container.lastChild)
+        
+            /**
+             * Render each image in the image set
+             */
+            for (let j = 0; j < imageSet.length; j++) {
+                console.log(`Rendering image ${j}`)
+                const image = imageSet[j];
+                createShowcase(image.image, image.numOfColumns)
+            }
+        
+            imageSetCounter++
+        }
+        
+        arrangeImageSets()
         renderDisplay()
+                
+        setInterval(renderDisplay, imageLifespan)
     },
     resize: function(date) {
         this.refresh(date)
